eb956e1875ef6d57370c9ef3a52c5b0c2df4eb8f
Crustasaur <crustasaur@kadam.app>
2026-02-08 04:43:47 +0000

    feat: implement daily step counting logic with persistence

--- mobile-app/lib/features/steps/data/repositories/step_repository_impl.dart
+++ mobile-app/lib/features/steps/data/repositories/step_repository_impl.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:injectable/injectable.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 import '../../domain/repositories/step_repository.dart';
 import '../datasources/step_local_data_source.dart';
 
@@ -13,6 +15,13 @@ class StepRepositoryImpl implements StepRepository {
   StreamSubscription<StepCount>? _stepSubscription;
   StreamSubscription<PedestrianStatus>? _statusSubscription;
 
+  static const String _keyStartOfDaySteps = 'sensor_value_at_start_of_day';
+  static const String _keyLastKnownDate = 'last_known_date';
+
+  int _startOfDaySteps = 0;
+  String? _lastKnownDate;
+  late SharedPreferences _prefs;
+
   StepRepositoryImpl(this._localDataSource);
 
   @override
@@ -23,6 +32,10 @@ class StepRepositoryImpl implements StepRepository {
 
   @override
   Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+    _startOfDaySteps = _prefs.getInt(_keyStartOfDaySteps) ?? -1;
+    _lastKnownDate = _prefs.getString(_keyLastKnownDate);
+
     final granted = await _localDataSource.requestPermission();
     if (granted) {
       _startListening();
@@ -34,7 +47,7 @@ class StepRepositoryImpl implements StepRepository {
   void _startListening() {
     _stepSubscription = _localDataSource.stepCountStream.listen(
       (stepCount) {
-        _stepController.add(stepCount.steps);
+        _handleStepUpdate(stepCount.steps);
       },
       onError: (error) {
         _statusController.add('Error: $error');
@@ -49,6 +62,29 @@ class StepRepositoryImpl implements StepRepository {
         _statusController.add('Status Error: $error');
       },
     );
+  }
+
+  void _handleStepUpdate(int currentSensorSteps) async {
+    final today = DateFormat('yyyy-MM-dd').format(DateTime.now());
+
+    if (_lastKnownDate != today) {
+      // It's a new day or first time initialization
+      _startOfDaySteps = currentSensorSteps;
+      _lastKnownDate = today;
+      
+      await _prefs.setInt(_keyStartOfDaySteps, _startOfDaySteps);
+      await _prefs.setString(_keyLastKnownDate, _lastKnownDate!);
+    }
+
+    // If the sensor value is less than _startOfDaySteps (e.g. phone rebooted on Android),
+    // we should reset _startOfDaySteps to avoid negative steps.
+    if (currentSensorSteps < _startOfDaySteps) {
+      _startOfDaySteps = currentSensorSteps;
+      await _prefs.setInt(_keyStartOfDaySteps, _startOfDaySteps);
+    }
+
+    final dailySteps = currentSensorSteps - _startOfDaySteps;
+    _stepController.add(dailySteps);
   }
   
   void dispose() {
